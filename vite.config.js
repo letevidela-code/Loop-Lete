855d0c8651d2afc8cd857c8e219b28f4dfe5177c
letevidela-code <letevidela@gmail.com>
2026-09-17 17:50:19 -0300

    Configurar compilación pública

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+    cssMinify: true,
+    reportCompressedSize: false,
+  },
+})
