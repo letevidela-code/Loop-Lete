6a54baf68aeb7e83ca5b211ea35307ba8afbc2ea
letevidela-code <letevidela@gmail.com>
2026-09-17 20:45:39 -0300

    Update profile-data.js

--- src/profile-data.js
+++ src/profile-data.js
@@ -1,3 +1,2 @@
-// En desarrollo se usa este fallback. El build público lo reemplaza automáticamente
-// por la foto exacta que ya está embebida en src/x7.jsx.
+
 export const PROFILE_IMAGE = 'https://avatars.githubusercontent.com/u/328251598?v=4'
